211e65f60c0d71577792d30384f4bedccd9c3bbf
Naoto SHINGAKI <nao.sora+github@gmail.com>
2016-04-13 19:16:17 +0900

    add post method

--- web.rb
+++ web.rb
@@ -16,3 +16,24 @@ get '/message' do
   text
 end
 
+post '/message' do
+  message = param[:message]
+  header = {
+    "Content-Type" => "application/json; charser=UTF-8",
+    'X-Line-ChannelID' => ENV['channel_id'],
+    'X-Line-ChannelSecret' => ENV['channel_secret'],
+    'X-Line-Trusted-User-With-ACL' => ENV['channel_mid']
+  }
+  body = {
+    to: ["送信相手のMID"],
+    toChannel:1383378250,
+    eventType:"138311608800106203",
+    content:{contentType:1, toType:1, text: message }
+  }
+
+  uri = URI.parse('https://trialbot-api.line.me')
+  client = Net::HTTP.new(uri.host, 443)
+  client.use_ssl = true
+  client.post("/v1/events", body.to_json, header)
+end
+
